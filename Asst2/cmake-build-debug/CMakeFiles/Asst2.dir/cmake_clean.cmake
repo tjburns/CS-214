6e751f3ca7e47e6ab821ae53be693151d8ef79a1
Tom Burns <tjburns28@gmail.com>
2019-03-24 20:21:32 -0400

    beginning asst2

---
+++ Asst2/cmake-build-debug/CMakeFiles/Asst2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Asst2.dir/main.c.obj"
+  "Asst2.pdb"
+  "Asst2.exe"
+  "Asst2.exe.manifest"
+  "libAsst2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Asst2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
